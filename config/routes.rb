5597d88ef3c82986a2756540cc503c5ec0231e77
Justin Bull <me@justinbull.ca>
2012-11-27 15:33:29 -0500

    fixed devise namespace collision

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 MyBikeLane::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :path_prefix => 'my'
   resources :users, :only => [:index, :show]
-  
+
   resources :violations
 
   # The priority is based upon order of creation:
