4eb3d5aa23c9c36a67340e76bef37187ad5d1886
Justin Bull <me@justinbull.ca>
2012-11-27 03:34:37 -0500

    email or username login support

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,10 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:login]
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
 
   validates_presence_of :username
   validates_format_of :username, :without => /@/, :message => "cannot contain the '@' symbol"
@@ -16,4 +13,14 @@ class User < ActiveRecord::Base
 
   # Virtual attribute for allowing email or username for logging in
   attr_accessor :login
+
+  # Overriding Devise's method for auth
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
