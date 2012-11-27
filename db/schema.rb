a9e836e4bf1708cd8cd98c5d2b43674d34f2a1fc
Justin Bull <me@justinbull.ca>
2012-11-27 03:12:05 -0500

    devise uses username instead of email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121127065120) do
+ActiveRecord::Schema.define(:version => 20121127080138) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20121127065120) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
