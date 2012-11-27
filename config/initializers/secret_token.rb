9bcbc705a39a25dd4f7fc41da1d4d1a8fae36e96
Justin Bull <me@justinbull.ca>
2012-11-26 22:59:06 -0500

    Empty app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBikeLane::Application.config.secret_token = 'ed09b1a20f87919799f217ac9a71cba4239066ab989e351901715d0c9926cf6057b889ed6e677ea3f8d93deb2e9d98f9a126f950b944e6fd1762217ecc94e221'
