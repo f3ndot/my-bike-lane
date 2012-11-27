4ffea2b19622a5d9920dc900a0ffb25b0142d0e3
Justin Bull <me@justinbull.ca>
2012-11-27 02:50:44 -0500

    gravatar navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def avatar_url(user, size = 48)
+    default_url = "#{root_url}/assets/images/default_avatar.jpg"
+    gravatar_id = Digest::MD5.hexdigest user.email.downcase
+    "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI.escape(default_url)}"
+  end
+
 end
